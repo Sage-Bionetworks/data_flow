c2aca5baefb29c9f4ac2f2685b6564a4c8db27d0
lakikowolfe <lauren.wolfe@sagebase.org>
2022-08-31 12:47:28 -0700

    remove dummy data

--- R/app_server.R
+++ R/app_server.R
@@ -120,59 +120,7 @@ app_server <- function( input, output, session ) {
   
   # DATASET DASH  #######################################################################
   
-  # dummy data
-  dataset_dash_data <- reactive({
-    
-    n <- 10
-    
-    dates<- lubridate::ymd(paste0("2022-", seq(3:12), "-01"))
-    
-    ds_names <- paste0(
-      sample(LETTERS, n, replace = TRUE),
-      sample(LETTERS, n, replace = TRUE),
-      "-unique-dataset-name")
-    
-    contributor <- sample(c("HTAN BU", "HTAN OHSU", "HTAN CHOP"), n, replace = TRUE)
-    dataset_type <- sample(c("BiospecimanBatch1", "PatientTrial1", "scATAC-seq", "FamilyHistory"), n, replace = TRUE)
-    dataset_name <- paste0(
-      sample(LETTERS, n, replace = TRUE),
-      sample(LETTERS, n, replace = TRUE),
-      "-unique-dataset-name")
-    num_items <- as.integer(runif(n, min = 5, max = 500))
-    release_scheduled <- sample(c(dates, NA), n, replace = TRUE)
-    embargo <- sample(c(dates, NA), n, replace = TRUE)
-    standard_compliance <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    qc_compliance <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    phi_detection_compliance <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    access_controls_compliance <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    data_portal <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    released <- sample(c(TRUE, FALSE), n, replace = TRUE)
-    
-    all_check_passing_row <- data.frame(contributor = "HTAN_OHSU",
-                                        dataset_name = "TF-unique-dataset-name",
-                                        dataset_type = "PatientTrial1",
-                                        num_items = 1,
-                                        release_scheduled = lubridate::floor_date(Sys.Date(), unit = "month"),
-                                        embargo = NA,
-                                        standard_compliance = TRUE,
-                                        data_portal = TRUE,
-                                        released = FALSE)
-
-        
-    df <- data.frame(contributor,
-                     dataset_name,
-                     dataset_type,
-                     num_items,
-                     release_scheduled,
-                     embargo,
-                     standard_compliance,
-                     data_portal,
-                     released)
-    
-    df <- rbind(df, all_check_passing_row)
-    
-    return(df)
-  })
+  
   
   # prepare data to be displayed by mod_datatable
 
