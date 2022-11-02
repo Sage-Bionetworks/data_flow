b93fb44fbb22a801598dda5a9b21143451171f72
lakikowolfe <lauren.wolfe@sagebase.org>
2022-11-02 13:29:42 -0700

    updates to manifest generation script

--- R/manifest.R
+++ R/manifest.R
@@ -3,19 +3,18 @@
 #' @export
 
 generate_data_flow_manifest <- function(storage_project_id,
-                                        config,
+                                        asset_view,
+                                        input_token,
                                         contributor = NULL,
+                                        calc_num_items = FALSE,
                                         testing = FALSE) {
-  
-  asset_view <- config$asset_view
-  token <- config$schematic_token
   
   # get all manifests for storage_project
   message(glue::glue("Building data flow status manifest for {storage_project_id}"))
   message("Getting all manifests")
   all_manifests_list <- storage_project_manifests(asset_view, 
                                                   storage_project_id, 
-                                                  token)
+                                                  input_token)
   
   # pull out data from list
   # dataset metadata
@@ -26,26 +25,33 @@ generate_data_flow_manifest <- function(storage_project_id,
   # component metadata
   dataset_component_metadata <- purrr::map(all_manifests_list, 3)
   dataset <- purrr::map_chr(dataset_component_metadata, 1)
+  dataset <- gsub("^$|^ $", "Not Applicable", dataset)
   
   # calculate number of items
   # pull down each manifest and count each row (1 file / row)
-  message("Counting items in each manifest")
-  num_items_list <- lapply(dataset_ids, function(id) {
-    manifest <- suppressMessages(manifest_download_to_df(config$asset_view,
-                                        id,
-                                        config$schematic_token))
-    if (!is.null(manifest)) {
-      nrow(manifest)
-    } else {
-      NA
-    }
-  })
-  
-  num_items <- purrr::flatten_int(num_items_list)
+  if (calc_num_items) {
+    message("Counting items in each manifest")
+    num_items_list <- lapply(dataset_ids, function(id) {
+      manifest <- suppressMessages(manifest_download_to_df(asset_view,
+                                                           id,
+                                                           input_token))
+      if (!is.null(manifest)) {
+        nrow(manifest)
+      } else {
+        NA
+      }
+    })
+    
+    num_items <- purrr::flatten_int(num_items_list)
+    
+  } else {
+    num_items <- rep("Not Applicable", length(dataset_ids))
+  }
   
   # create empty dataframe
   colnames <- c("Component", 
                 "contributor",
+                "entityId",
                 "dataset_name",
                 "dataset",
                 "num_items",
@@ -64,6 +70,7 @@ generate_data_flow_manifest <- function(storage_project_id,
   df$dataset_name <- dataset_names
   df$dataset <- dataset
   df$num_items <- num_items
+  df$entityId <- dataset_ids
   
   # add contributor
   if (!is.null(contributor)) {
@@ -81,12 +88,6 @@ generate_data_flow_manifest <- function(storage_project_id,
   
   return(df)
 }
-
-#' Convert date columns from string to date
-#'
-#' @param manifest A data flow status manifest. 
-#' 
-#' @export
 
 manifest_string_to_date <- function(manifest) {
   
