b88bf25d2c86222daab834af2bca13971fef3561
lakikowolfe <lauren.wolfe@sagebase.org>
2022-12-22 10:43:06 -0800

    unzip .venv

--- R/global.R
+++ R/global.R
@@ -1,3 +1,9 @@
+# unzip virtual environment, named as ".venv.zip"
+if (!file.exists(".venv")) utils::unzip(".venv.zip")
+
+# We get a '126' error (non-executable) if we don't do this:
+system("chmod -R +x .venv")
+
 # Activate virtual env
 Sys.unsetenv("RETICULATE_PYTHON")
 # Note, the name of the virtual environment is defined in the GH Actions workflow
@@ -8,8 +14,7 @@ reticulate::use_virtualenv(file.path(getwd(),venv_name), required = TRUE)
 
 
 if (interactive()) {
-  options(shiny.port = 3978,
-          launch.browser = T)
+  options(shiny.port = 3978)
 } 
 
 OAUTH_LIST <- projectlive.modules::create_oauth_list("inst/oauth_config.yml")
