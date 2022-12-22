8e9c74d582d2066fd1f034be647ebe0454baa5d5
lakikowolfe <lauren.wolfe@sagebase.org>
2022-12-22 14:17:24 -0800

    add some comments

--- R/global.R
+++ R/global.R
@@ -1,3 +1,8 @@
+## Set Up Virtual Environment
+# ShinyAppys has a limit of 7000 files which this app' grossly exceeds
+# due to its Python dependencies.  To get around the limit we zip up
+# the virtual environment before deployment and unzip it here.
+
 # unzip virtual environment, named as ".venv.zip"
 if (!file.exists(".venv")) utils::unzip(".venv.zip")
 
@@ -6,11 +11,9 @@ system("chmod -R +x .venv")
 
 # Activate virtual env
 Sys.unsetenv("RETICULATE_PYTHON")
-
-# We get a '126' error (non-executable) if we don't do this:
 reticulate::use_virtualenv(file.path(getwd(), ".venv"), required = TRUE)
 
-
+# set shiny port
 if (interactive()) {
   options(shiny.port = 3978)
 } 
