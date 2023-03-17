b5aed9f1ba5f1e12fcf2d5808536e958a4bb585c
lakikowolfe <lauren.wolfe@sagebase.org>
2023-03-17 15:06:25 -0700

    only run demo update

--- update_manifest_script.R
+++ update_manifest_script.R
@@ -21,29 +21,29 @@ tryCatch({
   }
 )
 
-
-# update INCLUDE
-tryCatch({
-  update_data_flow_manifest(asset_view = "syn50996463",
-                            manifest_dataset_id = "syn51060574",
-                            input_token = input_token,
-                            base_url = base_url)
-  },
-  error=function(e) {
-    message("Update to INCLUDE (syn50996463) failed")
-    message(e)
-  }
-)
-
-# update HTAN
-tryCatch({
-  update_data_flow_manifest(asset_view = "syn20446927",
-                            manifest_dataset_id = "syn38212343",
-                            input_token = input_token,
-                            base_url = base_url)
-  },
-  error=function(e) {
-    message("Update to HTAN (syn20446927) failed")
-    message(e)
-  }
-)
+# 
+# # update INCLUDE
+# tryCatch({
+#   update_data_flow_manifest(asset_view = "syn50996463",
+#                             manifest_dataset_id = "syn51060574",
+#                             input_token = input_token,
+#                             base_url = base_url)
+#   },
+#   error=function(e) {
+#     message("Update to INCLUDE (syn50996463) failed")
+#     message(e)
+#   }
+# )
+# 
+# # update HTAN
+# tryCatch({
+#   update_data_flow_manifest(asset_view = "syn20446927",
+#                             manifest_dataset_id = "syn38212343",
+#                             input_token = input_token,
+#                             base_url = base_url)
+#   },
+#   error=function(e) {
+#     message("Update to HTAN (syn20446927) failed")
+#     message(e)
+#   }
+# )
